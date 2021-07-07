d8169b3122231f65f4ad0ce691108f49fe07d530
ko0chan <kychan1320@gmail.com>
2021-07-07 22:06:30 +0900

    feat(HomeActivity.kt) : 전체 숙소 리스트 show

--- app/src/main/java/com/kychan/saveaccommodation/HomeActivity.kt
+++ app/src/main/java/com/kychan/saveaccommodation/HomeActivity.kt
@@ -3,12 +3,27 @@ package com.kychan.saveaccommodation
 import android.os.Bundle
 import com.iron.espresso.base.BaseActivity
 import com.kychan.saveaccommodation.databinding.ActivityHomeBinding
+import com.kychan.saveaccommodation.ui.AccommodationListFragment
 
 class HomeActivity : BaseActivity<ActivityHomeBinding>({ ActivityHomeBinding.inflate(it) }
 ) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        supportFragmentManager.beginTransaction()
+            .replace(
+                R.id.home_frg_container,
+                AccommodationListFragment.newInstance(),
+                resources.getString(R.string.accommodation_list)
+            )
+            .commit()
 
+        setView()
+    }
+
+    private fun setView() {
+        with(binding) {
+
+        }
     }
 }
