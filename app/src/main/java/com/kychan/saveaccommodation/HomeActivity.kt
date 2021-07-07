cdc5b33a015a9b957737ef604713db39d1a9f889
ko0chan <kychan1320@gmail.com>
2021-07-07 21:44:21 +0900

    feat(...) : base 적용 및 naming 수정

---
+++ app/src/main/java/com/kychan/saveaccommodation/HomeActivity.kt
@@ -0,0 +1,14 @@
+package com.kychan.saveaccommodation
+
+import android.os.Bundle
+import com.iron.espresso.base.BaseActivity
+import com.kychan.saveaccommodation.databinding.ActivityHomeBinding
+
+class HomeActivity : BaseActivity<ActivityHomeBinding>({ ActivityHomeBinding.inflate(it) }
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
