346aeb90af682ff39996cc9d20df07f7a918ef16
ko0chan <kychan1320@gmail.com>
2021-07-07 23:53:54 +0900

    feat(HomeActivity.kt) : tabLayout action 별 fragment 관리

--- app/src/main/java/com/kychan/saveaccommodation/HomeActivity.kt
+++ app/src/main/java/com/kychan/saveaccommodation/HomeActivity.kt
@@ -1,9 +1,12 @@
 package com.kychan.saveaccommodation
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
 import com.iron.espresso.base.BaseActivity
 import com.kychan.saveaccommodation.databinding.ActivityHomeBinding
 import com.kychan.saveaccommodation.ui.AccommodationFragment
+import com.kychan.saveaccommodation.ui.BookmarkFragment
 
 class HomeActivity : BaseActivity<ActivityHomeBinding>({ ActivityHomeBinding.inflate(it) }
 ) {
@@ -23,7 +26,51 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>({ ActivityHomeBinding.inf
 
     private fun setView() {
         with(binding) {
+            setSupportActionBar(toolbar)
+            val studyTabList = resources.getStringArray(R.array.home_tab)
+            studyTabList.forEach { title ->
+                bottomTab.addTab(bottomTab.newTab().setText(title))
+            }
 
+            bottomTab.run {
+                addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                    override fun onTabSelected(tab: TabLayout.Tab?) {
+                        tab ?: return
+
+                        val selectTabFragment = supportFragmentManager.findFragmentByTag("${tab.text}")
+                        if (selectTabFragment == null) {
+                            supportFragmentManager.beginTransaction()
+                                .add(R.id.home_frg_container, getFragment(tab.position), "${tab.text}")
+                                .commit()
+                        } else {
+                            supportFragmentManager.beginTransaction()
+                                .show(selectTabFragment)
+                                .commit()
+                        }
+                    }
+
+                    override fun onTabUnselected(tab: TabLayout.Tab?) {
+                        tab ?: return
+                        val hideFragment = supportFragmentManager.findFragmentByTag("${tab.text}")
+                        if (hideFragment != null) {
+                            supportFragmentManager.beginTransaction()
+                                .hide(hideFragment)
+                                .commit()
+                        }
+                    }
+
+                    override fun onTabReselected(tab: TabLayout.Tab?) {
+                    }
+                })
+            }
+        }
+    }
+
+    private fun getFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> AccommodationFragment.newInstance()
+            1 -> BookmarkFragment.newInstance()
+            else -> error("Invalid position")
         }
     }
 }
