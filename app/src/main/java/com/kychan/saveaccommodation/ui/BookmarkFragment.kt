6ec89a1cf90936e8030473efca87e9cf614c336d
ko0chan <kychan1320@gmail.com>
2021-07-07 23:54:14 +0900

    feat(BookmarkFragment.kt) : set toolbar title

--- app/src/main/java/com/kychan/saveaccommodation/ui/BookmarkFragment.kt
+++ app/src/main/java/com/kychan/saveaccommodation/ui/BookmarkFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.iron.espresso.base.BaseFragment
+import com.kychan.saveaccommodation.HomeActivity
+import com.kychan.saveaccommodation.R
 import com.kychan.saveaccommodation.databinding.FragmentBookmarkBinding
 
 class BookmarkFragment : BaseFragment<FragmentBookmarkBinding>() {
@@ -19,7 +21,13 @@ class BookmarkFragment : BaseFragment<FragmentBookmarkBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setView()
+    }
 
+    private fun setView() {
+        with(binding) {
+            (activity as HomeActivity).supportActionBar?.title = getString(R.string.bookmark)
+        }
     }
 
     companion object {
